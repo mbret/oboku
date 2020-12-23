19f4f83147944af96eed39c45d5b096ea8c5f4da
Maxime Bret <bret.maxime@gmail.com>
2020-12-24 01:07:16 +0900

    no message

--- src/download/states.ts
+++ src/download/states.ts
@@ -1,4 +1,4 @@
-import { atom, selectorFamily } from "recoil";
+import { atom, selector, selectorFamily } from "recoil";
 
 export enum DownloadState {
   None = 'none',
@@ -6,7 +6,11 @@ export enum DownloadState {
   Downloading = 'downloading'
 }
 
-export const normalizedBookDownloadsState = atom<Record<string, { downloadState?: DownloadState, downloadProgress?: number } | undefined>>({
+export const normalizedBookDownloadsState = atom<Record<string, {
+  downloadState?: DownloadState,
+  downloadProgress?: number,
+  size?: number,
+} | undefined>>({
   key: 'bookDownloadsState',
   default: {}
 })
@@ -16,5 +20,14 @@ export const bookDownloadsState = selectorFamily({
   get: (bookId: string) => ({ get }) => get(normalizedBookDownloadsState)[bookId] || {
     downloadState: DownloadState.None,
     downloadProgress: 0,
+  }
+})
+
+export const bookDownloadsSizeState = selector({
+  key: 'bookDownloadsSizeState',
+  get: ({ get }) => {
+    const books = Object.values(get(normalizedBookDownloadsState))
+
+    return books.filter(book => book?.downloadState === DownloadState.Downloaded).reduce((size, item) => size + (item?.size || 0), 0)
   }
 })
