36b416ff5418667027f9ff5e884cc51d544e76e5
Maxime Bret <bret.maxime@gmail.com>
2021-01-09 17:23:08 +0900

    show/hide arrow of list when needed

--- src/lists/ItemList.tsx
+++ src/lists/ItemList.tsx
@@ -1,14 +1,12 @@
-import React, { useMemo, FC, ComponentProps, useRef, useState, useEffect } from 'react'
+import React, { useMemo, FC, ComponentProps, useRef, useEffect, useState, useCallback } from 'react'
 import { useScrollbarWidth, useMeasure } from 'react-use';
 import { RecyclerListView, DataProvider, LayoutProvider } from "recyclerlistview/web";
 import { ArrowBackIosRounded, ArrowForwardIosRounded, ExpandMoreRounded, ExpandLessRounded } from '@material-ui/icons';
-import { TOnItemStatusChanged } from 'recyclerlistview/dist/web/core/ViewabilityTracker';
-import { ScrollEvent } from 'recyclerlistview/dist/web/core/scrollcomponent/BaseScrollView';
 import { makeStyles } from '@material-ui/core';
 import { Report } from '../report';
 
-//Create the data provider and provide method which takes in two rows of data and return if those two are different or not.
-//THIS IS VERY IMPORTANT, FORGET PERFORMANCE IF THIS IS MESSED UP
+// Create the data provider and provide method which takes in two rows of data and return if those two are different or not.
+// THIS IS VERY IMPORTANT, FORGET PERFORMANCE IF THIS IS MESSED UP
 const initialDataProvider = new DataProvider((r1, r2) => {
   return r1 !== r2;
 })
@@ -28,19 +26,21 @@ export const ItemList: FC<{
   const classes = useClasses()
   const sbw = useScrollbarWidth() || 0;
   const listRef = useRef<RecyclerListView<any, any>>()
-  const [ref, { width }] = useMeasure();
+  const [ref, { width, height }] = useMeasure();
   const maxWidth = (width - sbw)
   const hasHeader = !!renderHeader
   const computedItemWidth = itemWidth ? itemWidth : Math.floor(maxWidth / itemsPerRow)
   const computedItemHeight = itemHeight || computedItemWidth / preferredRatio
   const displayScrollerButtons = hasHeader ? data.length > 1 : data.length > 0
+  const [showBackwardArrow, setShowBackwardArrow] = useState(false)
+  const [showForwardArrow, setShowForwardArrow] = useState(true)
 
-  //Create the layout provider
-  //First method: Given an index return the type of item e.g ListItemType1, ListItemType2 in case you have variety of items in your list/grid
-  //Second: Given a type and object set the exact height and width for that type on given object, if you're using non deterministic rendering provide close estimates
-  //If you need data based check you can access your data provider here
-  //You'll need data in most cases, we don't provide it by default to enable things like data virtualization in the future
-  //NOTE: For complex lists LayoutProvider will also be complex it would then make sense to move it to a different file
+  // Create the layout provider
+  // First method: Given an index return the type of item e.g ListItemType1, ListItemType2 in case you have variety of items in your list/grid
+  // Second: Given a type and object set the exact height and width for that type on given object, if you're using non deterministic rendering provide close estimates
+  // If you need data based check you can access your data provider here
+  // You'll need data in most cases, we don't provide it by default to enable things like data virtualization in the future
+  // NOTE: For complex lists LayoutProvider will also be complex it would then make sense to move it to a different file
   const layoutProvider = useMemo(() => new LayoutProvider(
     index => hasHeader && index === 0 ? 'header' : 'item',
     (type, dim) => {
@@ -59,7 +59,7 @@ export const ItemList: FC<{
   ), [computedItemWidth, computedItemHeight, hasHeader, headerHeight, maxWidth])
 
   /**
-   * Ugly hack but because we don't want to use the window for scrol and resize (since we have custom dimension)
+   * Ugly hack but because we don't want to use the window for scroll and resize (since we have custom dimension)
    * the resize is not being listened to (https://github.com/Flipkart/recyclerlistview/blob/5500b6bbfd098868c11db83311d3d35ae742f991/src/platform/web/scrollcomponent/ScrollViewer.tsx#L28)
    * It is only being listened when it's on window. Anyway we have a dynamic dimensions so we just force a resize whenever
    * the width change. It's using internal API and is not safe. One day I will need to create my own external view or fix the
@@ -75,8 +75,6 @@ export const ItemList: FC<{
       }
     }
   }, [maxWidth])
-
-  console.log('LAYOUT', computedItemWidth, computedItemWidth * itemsPerRow, maxWidth, itemWidth)
 
   const getOffsets = () => isHorizontal
     ? data.map((_, i) => Math.floor(listRef?.current?.getLayout(i)?.x || 0))
@@ -114,8 +112,45 @@ export const ItemList: FC<{
     }
   }
 
+  const onScroll = useCallback((e: Parameters<NonNullable<ComponentProps<typeof RecyclerListView>['onScroll']>>[0]) => {
+    const { x, y } = e.nativeEvent.contentOffset
+    if ((isHorizontal && x > 0) || (!isHorizontal && y > 0)) {
+      setShowBackwardArrow(true)
+    } else {
+      setShowBackwardArrow(false)
+    }
+    setShowForwardArrow(true)
+  }, [isHorizontal])
+
   // @ts-ignore
   window.list = listRef
+
+  /**
+   * onEndReached is not reliable (fired as soon as the list is created..) 
+   * and onScroll is only called when scrolling
+   * so in order to show/hide the arrows as much as we can we also listen to 
+   * changes in measure and manually check the content
+   * */ 
+  useEffect(() => {
+    const contentDimension = listRef.current?.getContentDimension()
+    if (isHorizontal && (contentDimension?.width || 0) <= width) {
+      setShowForwardArrow(false)
+    } else if (!isHorizontal && (contentDimension?.height || 0) <= height) {
+      setShowForwardArrow(false)
+    } else {
+      setShowForwardArrow(true)
+    }
+  }, [height, width, isHorizontal])
+
+  const scrollViewProps = useMemo(() => ({
+    showsVerticalScrollIndicator: false,
+    useWindowScroll: false,
+    canChangeSize: true,
+  }), [])
+
+  const onEndReached = useCallback(() => {
+    setShowForwardArrow(false)
+  }, [])
 
   return (
     <div style={{
@@ -132,16 +167,14 @@ export const ItemList: FC<{
           rowRenderer={rowRenderer}
           canChangeSize={true}
           isHorizontal={isHorizontal}
-          scrollViewProps={{
-            showsVerticalScrollIndicator: false,
-            useWindowScroll: false,
-            canChangeSize: true,
-          }}
+          onScroll={onScroll}
+          onEndReached={onEndReached}
+          scrollViewProps={scrollViewProps}
         />
       )}
       {displayScrollerButtons && (
         <>
-          {!isHorizontal && (
+          {!isHorizontal && showBackwardArrow && (
             <div
               className={`${classes.verticalScrollButton} ${classes.verticalScrollButtonLess}`}
               onClick={onExpandLessClick}
@@ -149,7 +182,7 @@ export const ItemList: FC<{
               <ExpandLessRounded style={{ color: 'white' }} />
             </div>
           )}
-          {!isHorizontal && (
+          {!isHorizontal && showForwardArrow && (
             <div
               className={`${classes.verticalScrollButton} ${classes.verticalScrollButtonMore}`}
               onClick={onExpandMoreClick}
@@ -157,7 +190,7 @@ export const ItemList: FC<{
               <ExpandMoreRounded style={{ color: 'white' }} />
             </div>
           )}
-          {isHorizontal && (
+          {isHorizontal && showBackwardArrow && (
             <div
               style={{
                 position: 'absolute',
@@ -169,7 +202,7 @@ export const ItemList: FC<{
               <ArrowBackIosRounded style={{ color: 'white' }} />
             </div>
           )}
-          {isHorizontal && (
+          {isHorizontal && showForwardArrow && (
             <div
               style={{
                 position: 'absolute',
@@ -188,12 +221,9 @@ export const ItemList: FC<{
 }
 
 const useClasses = makeStyles((theme) => {
-  type Props = { isHorizontal: boolean, windowSize: { width: number } }
-
   return {
     verticalScrollButton: {
       position: 'absolute',
-      // position: 'fixed',
       padding: theme.spacing(1),
       backgroundColor: 'gray',
       opacity: 0.5,
