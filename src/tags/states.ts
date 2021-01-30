43cb62e2fa5fa10870b1153f8775f9b74d8adf00
Maxime Bret <bret.maxime@gmail.com>
2021-01-30 12:39:55 +0900

    no message

--- src/tags/states.ts
+++ src/tags/states.ts
@@ -1,9 +1,25 @@
-import { atom, selector } from "recoil"
+import { atom, selector, selectorFamily } from "recoil"
 import { TagsDocType } from "oboku-shared"
+import { bookIdsState } from "../books/states"
 
 export const normalizedTagsState = atom<Record<string, TagsDocType | undefined>>({
   key: 'tagsState',
   default: {}
+})
+
+export const tagState = selectorFamily({
+  key: 'tagState',
+  get: (id: string) => ({ get }) => {
+    const tag = get(normalizedTagsState)[id]
+    const bookIds = get(bookIdsState)
+
+    if (!tag) return undefined
+
+    return {
+      ...tag,
+      books: tag.books.filter(id => bookIds.includes(id))
+    }
+  },
 })
 
 export const protectedTagIdsState = selector({
@@ -19,7 +35,7 @@ export const tagsAsArrayState = selector<TagsDocType[]>({
   key: 'tagsAsArrayState',
   get: ({ get }) => {
     const tags = get(normalizedTagsState)
-    
-    return Object.values(tags) as NonNullable<typeof tags[number]>[]
+
+    return Object.keys(tags).map(id => get(tagState(id))) as NonNullable<typeof tags[number]>[]
   }
 })
