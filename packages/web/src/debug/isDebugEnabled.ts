734c395b0df26e3f941f61cf24059efa51bbef74
Maxime Bret <bret.maxime@gmail.com>
2021-05-06 10:39:46 +0900

    fixed service worker localstorage

--- packages/web/src/debug/isDebugEnabled.ts
+++ packages/web/src/debug/isDebugEnabled.ts
@@ -1,3 +1,6 @@
 export const isDebugEnabled = () => {
+  // @handle service worker with session storage instead
+  if (!window.localStorage) return false
+  
   return localStorage.getItem('oboku_debug_enabled') === 'true'
 }
