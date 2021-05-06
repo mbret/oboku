d807828fe505aed1bbf491d423e2d2ef05e76ba7
Maxime Bret <bret.maxime@gmail.com>
2021-05-06 11:42:46 +0900

    no message

--- packages/web/src/debug/isDebugEnabled.ts
+++ packages/web/src/debug/isDebugEnabled.ts
@@ -1,6 +1,6 @@
 export const isDebugEnabled = () => {
-  // @handle service worker with session storage instead
-  if (!window.localStorage) return false
-  
+  // @todo handle service worker with session storage instead
+  if (typeof window === 'undefined' || !window.localStorage) return false
+
   return localStorage.getItem('oboku_debug_enabled') === 'true'
 }
