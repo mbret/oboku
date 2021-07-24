e9ea86ee50e523fce8bbea095064f44f4b41833c
Maxime Bret <bret.maxime@gmail.com>
2021-07-24 14:04:32 +0900

    some update deps

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -1,2 +1,17 @@
 export type PromiseReturnType<T extends (...args: any) =>
-Promise<any>> = T extends (...args: any) => Promise<infer U> ? U : any;
+  Promise<any>> = T extends (...args: any) => Promise<infer U> ? U : any;
+
+/**
+ * @link https://stackoverflow.com/a/49670389/3443137
+ */
+type DeepReadonly<T> =
+  T extends (infer R)[] ? DeepReadonlyArray<R> :
+  T extends Function ? T :
+  T extends object ? DeepReadonlyObject<T> :
+  T;
+
+export interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> { }
+
+export type DeepReadonlyObject<T> = {
+  readonly [P in keyof T]: DeepReadonly<T[P]>;
+};
