67f161c13a853ccf4c7ac6fcbfad4931efdef7d5
Maxime Bret <bret.maxime@gmail.com>
2021-11-11 21:38:46 +0900

    fixed debug resolver for sw

--- packages/web/src/debug/isDebugEnabled.shared.ts
+++ packages/web/src/debug/isDebugEnabled.shared.ts
@@ -1,8 +1,7 @@
-let isEnabled = localStorage.getItem('oboku_debug_enabled') === 'true'
+let isEnabled = (typeof window === 'undefined' || !window.localStorage)
+  ? false // @todo handle service worker with session storage instead
+  : localStorage.getItem('oboku_debug_enabled') === 'true'
 
 export const isDebugEnabled = () => {
-  // @todo handle service worker with session storage instead
-  if (typeof window === 'undefined' || !window.localStorage) return false
-
   return isEnabled || process.env.NODE_ENV === `development`
 }
