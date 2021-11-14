d4aec7ff2617b754387a17923b0cdb6e9048cff7
Maxime Bret <bret.maxime@gmail.com>
2021-11-14 12:37:34 +0900

    upgraded some deps

--- packages/shared/dist/validators.d.ts
+++ packages/shared/dist/validators.d.ts
@@ -1,24 +1,24 @@
 import * as yup from 'yup';
 export declare const signinSchema: yup.ObjectSchema<import("yup/lib/object").Assign<import("yup/lib/object").ObjectShape, {
-    email: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
-    password: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
-}>, Record<string, any>, import("yup/lib/object").TypeOfShape<import("yup/lib/object").Assign<import("yup/lib/object").ObjectShape, {
-    email: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
-    password: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
+    email: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+    password: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+}>, import("yup/lib/object").AnyObject, import("yup/lib/object").TypeOfShape<import("yup/lib/object").Assign<import("yup/lib/object").ObjectShape, {
+    email: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+    password: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
 }>>, import("yup/lib/object").AssertsShape<import("yup/lib/object").Assign<import("yup/lib/object").ObjectShape, {
-    email: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
-    password: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
+    email: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+    password: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
 }>>>;
 export declare const signupSchema: yup.ObjectSchema<import("yup/lib/object").Assign<import("yup/lib/object").ObjectShape, {
-    email: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
-    password: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
-    code: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
-}>, Record<string, any>, import("yup/lib/object").TypeOfShape<import("yup/lib/object").Assign<import("yup/lib/object").ObjectShape, {
-    email: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
-    password: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
-    code: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
+    email: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+    password: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+    code: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+}>, import("yup/lib/object").AnyObject, import("yup/lib/object").TypeOfShape<import("yup/lib/object").Assign<import("yup/lib/object").ObjectShape, {
+    email: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+    password: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+    code: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
 }>>, import("yup/lib/object").AssertsShape<import("yup/lib/object").Assign<import("yup/lib/object").ObjectShape, {
-    email: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
-    password: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
-    code: import("yup/lib/string").RequiredStringSchema<string, Record<string, any>>;
+    email: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+    password: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+    code: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
 }>>>;
