a07a37f07741f152683c9fba8ecc47277c853993
Maxime Bret <bret.maxime@gmail.com>
2023-02-11 11:51:28 +0100

    fix: fixed missed cancellation of popup

--- packages/web/src/plugins/google/useRemoveBook.ts
+++ packages/web/src/plugins/google/useRemoveBook.ts
@@ -3,7 +3,6 @@ import { ObokuPlugin, ObokuPluginError } from "@oboku/plugin-front"
 import { extractIdFromResourceId } from "./lib/helpers"
 import { useAccessToken } from "./lib/useAccessToken"
 import { useGoogle } from "./lib/useGsiClient"
-import { isPopupClosedError } from "./lib/errors"
 
 export const useRemoveBook: ObokuPlugin[`useRemoveBook`] = ({
   requestPopup
@@ -13,27 +12,19 @@ export const useRemoveBook: ObokuPlugin[`useRemoveBook`] = ({
 
   return useCallback(
     async (link) => {
-      try {
-        await requestToken({
-          scope: [`https://www.googleapis.com/auth/drive`]
-        })
+      await requestToken({
+        scope: [`https://www.googleapis.com/auth/drive`]
+      })
 
-        const gapi = await lazyGapi
+      const gapi = await lazyGapi
 
-        const fileId = extractIdFromResourceId(link.resourceId)
+      const fileId = extractIdFromResourceId(link.resourceId)
 
-        await gapi.client.drive.files.delete({
-          fileId
-        })
+      await gapi.client.drive.files.delete({
+        fileId
+      })
 
-        return { data: {} }
-      } catch (e) {
-        if (isPopupClosedError(e)) {
-          throw new ObokuPluginError({ code: "cancelled" })
-        }
-
-        throw e
-      }
+      return { data: {} }
     },
     [requestToken, lazyGapi]
   )
