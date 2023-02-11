b8ce9e2363185762c919ff24989cf170666a53a4
Maxime Bret <bret.maxime@gmail.com>
2023-02-11 10:25:56 +0100

    fix: re-enable sentry on production

--- packages/web/src/debug/index.ts
+++ packages/web/src/debug/index.ts
@@ -1,57 +1,46 @@
-// import * as Sentry from "@sentry/react";
-// import { Integrations } from "@sentry/tracing";
+import * as Sentry from "@sentry/react"
+import { BrowserTracing } from "@sentry/tracing"
 // import firebase from "firebase/compat/app"
 // import { getAnalytics } from "firebase/analytics";
 // import { FIREBASE_BASE_CONFIG } from '../constants'
 import ReactGA from "react-ga"
-// import { version } from '../../package.json'
+import { version } from "../../package.json"
 import localforage from "localforage"
 import { isDebugEnabled } from "./isDebugEnabled.shared"
 import "./reportWebVitals"
+import { SENTRY_DSN } from "../constants.shared"
 
 ReactGA.initialize("UA-43281094-4")
 
 // @ts-ignore
 window.localforage = localforage
 
-if (!import.meta.env.DEV) {
-  // Sentry.init({
-  //   dsn: "https://0d7a61df8dba4122be660fcc1161bf49@o490447.ingest.sentry.io/5554285",
-  //   autoSessionTracking: true,
-  //   integrations: [
-  //     new Integrations.BrowserTracing(),
-  //   ],
-  //   beforeSend(event) {
-  //     if (event?.extra?.feedback) {
-  //       Sentry.showReportDialog({
-  //         labelSubmit: 'Submit feedback',
-  //         title: 'Want to report a bug or give us a sugestion?',
-  //         subtitle: 'Your feedback is very valuable. Do not hesitate to abuse this form!',
-  //         subtitle2: '',
-  //         user: {
-  //           email: (event?.extra?.email as string | undefined),
-  //           name: (event?.extra?.name as string | undefined),
-  //         }
-  //       })
-  //     }
+Sentry.init({
+  dsn: SENTRY_DSN,
+  environment: import.meta.env.MODE,
+  enabled: !import.meta.env.DEV,
+  autoSessionTracking: true,
+  integrations: [new BrowserTracing()],
+  release: version,
+  beforeSend(event) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      Sentry.showReportDialog({ eventId: event.event_id })
+    }
 
-  //     return event
-  //   },
-  //   // We recommend adjusting this value in production, or using tracesSampler
-  //   // for finer control
-  //   tracesSampleRate: 1.0,
-  // })
-  // Sentry.setTag('npm_version', version)
+    return event
+  },
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0
+})
 
-  // firebase.initializeApp(FIREBASE_BASE_CONFIG)
-  // getAnalytics()
+// firebase.initializeApp(FIREBASE_BASE_CONFIG)
+// getAnalytics()
 
-  if (!isDebugEnabled()) {
-    console.log = () => {}
-    console.warn = () => {}
-  }
-} else {
-  console.log(import.meta.env)
+if (!isDebugEnabled()) {
+  console.log = () => {}
+  console.warn = () => {}
 }
 
 export const toggleDebug = () => {
