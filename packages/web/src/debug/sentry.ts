5ff2bda242416f3bbae585cc166a3e8015dcd987
Maxime Bret <bret.maxime@gmail.com>
2023-02-11 11:45:39 +0100

    fix: fixed console error

--- packages/web/src/debug/sentry.ts
+++ packages/web/src/debug/sentry.ts
@@ -1,15 +1,19 @@
-import { BrowserTracing } from "@sentry/tracing"
 import { version } from "../../package.json"
 import "./reportWebVitals"
 import { SENTRY_DSN } from "../constants.shared"
 import { init, showReportDialog } from "@sentry/react"
+import { CaptureConsole } from "@sentry/integrations"
 
 init({
   dsn: SENTRY_DSN,
   environment: import.meta.env.MODE,
   enabled: !import.meta.env.DEV,
   autoSessionTracking: true,
-  integrations: [new BrowserTracing()],
+  integrations: [
+    new CaptureConsole({
+      levels: ["error"]
+    })
+  ],
   release: version,
   beforeSend(event) {
     // Check if it is an exception, and if so, show the report dialog
