c97b506dbbcabc9c631d31d8253d5d55df15c998
Maxime Bret <bret.maxime@gmail.com>
2023-02-11 11:51:47 +0100

    feat: removed unused code

--- packages/web/src/plugins/google/lib/errors.ts
+++
@@ -1,8 +0,0 @@
-export const isPopupClosedError = (error: unknown) => {
-  return (
-    error &&
-    typeof error === "object" &&
-    "type" in error &&
-    error.type === "popup_closed"
-  )
-}
