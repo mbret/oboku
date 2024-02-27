c2ca8934a7a877d9fb15451e694652d75adb3585
maxime <bret.maxime@gmail.com>
2024-02-27 22:52:41 +0100

    feat: disable viewmode

--- packages/web/src/library/collections/FilterBar.tsx
+++ packages/web/src/library/collections/FilterBar.tsx
@@ -1,15 +1,13 @@
-import { useSignalValue } from "reactjrx"
 import { ListActionsToolbar } from "../../common/lists/ListActionsToolbar"
 import { FiltersDrawer } from "./FiltersDrawer"
 import { useCallback, useState } from "react"
-import { collectionsListSignal } from "./state"
 
 export const FilterBar = () => {
   const [isFiltersDrawerOpen, setIsFiltersDrawerOpen] = useState(false)
-  const viewMode = useSignalValue(
-    collectionsListSignal,
-    (state) => state.viewMode
-  )
+  // const viewMode = useSignalValue(
+  //   collectionsListSignal,
+  //   (state) => state.viewMode
+  // )
 
   const onFiltersDrawerClose = useCallback(() => {
     setIsFiltersDrawerOpen(false)
@@ -21,13 +19,13 @@ export const FilterBar = () => {
         onFilterClick={() => {
           setIsFiltersDrawerOpen(true)
         }}
-        onViewModeChange={(value) => {
-          collectionsListSignal.setValue((state) => ({
-            ...state,
-            viewMode: value
-          }))
-        }}
-        viewMode={viewMode}
+        // onViewModeChange={(value) => {
+        //   collectionsListSignal.setValue((state) => ({
+        //     ...state,
+        //     viewMode: value
+        //   }))
+        // }}
+        // viewMode={viewMode}
       />
       <FiltersDrawer
         onClose={onFiltersDrawerClose}
