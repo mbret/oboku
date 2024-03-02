fcf25e17dd4b7e7e6d9007c19c48b48176a2253e
maxime <bret.maxime@gmail.com>
2024-03-02 14:34:33 +0100

    fix: make sure to not crash if one provider crash

--- packages/api/src/libs/metadata/getBookSourcesMetadata.ts
+++ packages/api/src/libs/metadata/getBookSourcesMetadata.ts
@@ -1,10 +1,25 @@
 import { Metadata } from "./types"
 import { getGoogleMetadata } from "./google/getGoogleMetadata"
+import { Logger } from "@libs/logger"
+
+const swallowError = async <T>(promise: Promise<T>) => {
+  try {
+    return await promise
+  } catch (e) {
+    Logger.error(e)
+  }
+}
 
 export const getBookSourcesMetadata = async (
   metadata: Metadata
 ): Promise<Metadata[]> => {
-  const google = await getGoogleMetadata(metadata)
+  const list = []
 
-  return [google]
+  const google = await swallowError(getGoogleMetadata(metadata))
+
+  if (google) {
+    list.push(google)
+  }
+
+  return list
 }
