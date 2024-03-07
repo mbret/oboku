99ef304357aaaa328347ed97eeb9e91a6f57674f
maxime <bret.maxime@gmail.com>
2024-03-07 12:31:57 +0100

    feat: added lock on sync datasource

--- packages/api/src/functions/syncDataSource/handler.ts
+++ packages/api/src/functions/syncDataSource/handler.ts
@@ -4,11 +4,17 @@ import { getNormalizedHeader } from "@libs/utils"
 import { STAGE } from "../../constants"
 import schema from "./schema"
 import { InvokeCommand } from "@aws-sdk/client-lambda"
+import { Logger } from "@libs/logger"
+import { supabase } from "@libs/supabase/client"
+import { isLockOutdated } from "@libs/supabase/isLockOutdated"
+
+const LOCK_MAX_DURATION_MN = 10
 
 const lambda: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
   event
 ) => {
-  const lambda = getAwsLambda()
+  const client = getAwsLambda()
+
   const command = new InvokeCommand({
     InvocationType: "Event",
     FunctionName: `oboku-api-${STAGE}-syncDataSourceLongProcess`,
@@ -21,7 +27,44 @@ const lambda: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
     })
   })
 
-  await lambda.send(command)
+  const lockId = `sync_${event.body.bookId}`
+
+  const response = await supabase
+    .from("lock")
+    .insert({ id: 1, lock_id: lockId })
+    .select()
+
+  if (response.status === 409) {
+    const response = await supabase.from("lock").select().eq("lock_id", lockId)
+
+    const lock = (response.data ?? [])[0]
+    const now = new Date()
+
+    if (isLockOutdated(lock, LOCK_MAX_DURATION_MN)) {
+      Logger.log(`${lockId} lock is outdated and will be recreated`)
+
+      const updatedResponse = await supabase
+        .from("lock")
+        .upsert({ id: lock.id, created_at: now, lock_id: lockId })
+        .select()
+
+      if (updatedResponse.status === 200) {
+        Logger.log(`${lockId} lock correctly updated, command will be sent`)
+
+        await client.send(command)
+      }
+    } else {
+      Logger.log(`${lockId} invocation is ignored`)
+    }
+  }
+
+  if (response.status === 201) {
+    Logger.log(
+      `New lock created for ${lockId} with id ${(response.data ?? [])[0].id}. Command will be sent`
+    )
+
+    await client.send(command)
+  }
 
   return {
     statusCode: 202,
