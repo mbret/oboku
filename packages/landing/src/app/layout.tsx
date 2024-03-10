4a8658109e9245babeeb41dfe02c22eae5a69b56
maxime <bret.maxime@gmail.com>
2024-03-10 13:28:09 +0100

    fix: fixed font

--- packages/landing/src/app/layout.tsx
+++ packages/landing/src/app/layout.tsx
@@ -21,6 +21,16 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+        />
       </head>
       <body className={inter.className}>
         <CssBaseline />
