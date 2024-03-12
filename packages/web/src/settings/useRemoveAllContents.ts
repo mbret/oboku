c2f5d8ec8eb9605c637161c4ef1573c7aa3f451b
maxime <bret.maxime@gmail.com>
2024-03-12 22:02:54 +0100

    feat: added reset all content dialog

--- packages/web/src/settings/useRemoveAllContents.ts
+++ packages/web/src/settings/useRemoveAllContents.ts
@@ -1,48 +1,107 @@
 import { useMutation } from "reactjrx"
 import { getLatestDatabase } from "../rxdb/useCreateDatabase"
-import { catchError, from, mergeMap, tap } from "rxjs"
+import { catchError, combineLatest, from, map, mergeMap, of, tap } from "rxjs"
 import { useSyncReplicate } from "../rxdb/replication/useSyncReplicate"
 import { useLock } from "../common/BlockingBackdrop"
+import { useDialogManager } from "../dialog"
+import { authorizeAction } from "../auth/AuthorizeActionDialog"
+import { Report } from "../debug/report.shared"
+import { CancelError } from "../errors"
 
 export const useRemoveAllContents = () => {
   const { mutateAsync: sync } = useSyncReplicate()
   const [lock] = useLock()
+  const dialog = useDialogManager()
 
   return useMutation({
-    onMutate: () => {
-      lock()
-    },
     mutationFn: () =>
       getLatestDatabase().pipe(
-        mergeMap((database) =>
-          from(
-            Promise.all([
-              database.book.find().remove(),
-              database.obokucollection.find().remove(),
-              database.link.find().remove(),
-              database.tag.find().remove(),
-              database.datasource.remove()
-            ])
-          ).pipe(
-            mergeMap(() =>
-              from(
-                sync([
-                  database.book,
-                  database.obokucollection,
-                  database.link,
-                  database.tag,
-                  database.datasource
-                ])
+        mergeMap((db) =>
+          combineLatest([
+            of(db),
+            from(db.book.count().exec()),
+            from(db.obokucollection.count().exec()),
+            from(db.tag.count().exec()),
+            from(db.datasource.count().exec())
+          ])
+        ),
+        mergeMap(
+          ([
+            database,
+            bookCount,
+            collectionCount,
+            tagCount,
+            dataSourceCount
+          ]) => {
+            const confirmed$ = from(
+              new Promise<void>((resolve, reject) => {
+                dialog({
+                  title: "Account reset",
+                  content: `This action will remove all of your content. Here is a breakdown of everything that will be removed:\n 
+                ${bookCount} books, ${collectionCount} collections, ${tagCount} tags and ${dataSourceCount} data sources. \n\nThis operation can take a long time and you NEED to be connected to internet`,
+                  canEscape: true,
+                  cancellable: true,
+                  onConfirm: resolve,
+                  onCancel() {
+                    reject(new CancelError())
+                  }
+                })
+              })
+            )
+
+            return confirmed$.pipe(
+              mergeMap(() =>
+                from(
+                  new Promise<void>((resolve, reject) =>
+                    authorizeAction(resolve, () => reject(new CancelError()))
+                  )
+                )
+              ),
+              map(() => lock()),
+              mergeMap((unlock) =>
+                from(
+                  Promise.all([
+                    database.book.find().remove(),
+                    database.obokucollection.find().remove(),
+                    database.link.find().remove(),
+                    database.tag.find().remove(),
+                    database.datasource.find().remove()
+                  ])
+                ).pipe(
+                  mergeMap(() =>
+                    from(
+                      sync([
+                        database.book,
+                        database.obokucollection,
+                        database.link,
+                        database.tag,
+                        database.datasource
+                      ])
+                    )
+                  ),
+                  catchError((e) => {
+                    unlock()
+
+                    throw e
+                  })
+                )
               )
             )
-          )
+          }
         ),
         tap(() => {
           window.location.reload()
         }),
         catchError((e) => {
-          console.error(e)
+          if (e instanceof CancelError) return of(null)
 
+          Report.error(e)
+
+          dialog({
+            title: "Something went wrong!",
+            content:
+              "Something went wrong during the process. No need to panic since you already wanted to destroy everything anyway. If everything is gone, you should not worry too much, if you still have contents, try to do it again"
+          })
           throw e
         })
       )
