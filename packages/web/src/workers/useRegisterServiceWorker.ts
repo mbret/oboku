4cd58ffac01d9d5fdb40a471fe3ea58622709910
Maxime Bret <bret.maxime@gmail.com>
2024-07-28 18:38:55 +0200

    fix: cleanup

--- packages/web/src/workers/useRegisterServiceWorker.ts
+++ packages/web/src/workers/useRegisterServiceWorker.ts
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from "react"
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
+import { Report } from "../debug/report.shared"
 
 export const useRegisterServiceWorker = () => {
   const [waitingWorker, setWaitingWorker] = useState<ServiceWorker | undefined>(
@@ -11,7 +12,9 @@ export const useRegisterServiceWorker = () => {
     if (firstTime.current) {
       firstTime.current = false
       serviceWorkerRegistration.register({
-        onSuccess: () => console.warn("onSuccess"),
+        onSuccess: () => {
+          Report.info(`service worker registered with success!`)
+        },
         onUpdate: (reg) => reg.waiting && setWaitingWorker(reg.waiting),
         onWaitingServiceWorkerFound: async (reg) => {
           reg.waiting && setWaitingWorker(reg.waiting)
