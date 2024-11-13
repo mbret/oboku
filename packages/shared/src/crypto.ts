14760be9fb3b38f18ca3c90823e34531c244e5cd
mbret <bret.maxime@gmail.com>
2024-11-14 00:14:56 +0100

    feat: removed crypto

--- packages/shared/src/crypto.ts
+++ packages/shared/src/crypto.ts
@@ -1,10 +1,17 @@
-import sha256 from "crypto-js/sha256"
-
 /**
  * The contentPassword is for user content protection. It is made to work
  * offline and therefore is not secured yet. This is not intended for
  * fully secured solution
  */
-export const hashContentPassword = (password: string) => {
-  return sha256(password).toString()
+export const hashContentPassword = async (password: string) => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(password)
+
+  const buffer = await window.crypto.subtle.digest(`SHA-256`, data)
+
+  // Convert ArrayBuffer to hex string
+  const hashArray = Array.from(new Uint8Array(buffer))
+  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("")
+
+  return hashHex
 }
