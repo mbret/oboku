a7cb16af0813a8d867accc956909f9134a44a192
mbret <bret.maxime@gmail.com>
2025-02-25 23:50:09 +0100

    fix: lint

--- packages/web/stream-shim.js
+++ packages/web/stream-shim.js
@@ -1,7 +1,7 @@
 // biome-ignore lint/complexity/useArrowFunction: <explanation>
 // biome-ignore lint/style/useConst: <explanation>
-export let stream = { Stream: function () {} };
+export let stream = { Stream: function () {} }
 
 // biome-ignore lint/complexity/useArrowFunction: <explanation>
 // biome-ignore lint/style/useConst: <explanation>
-export let Stream = function () {};
+export let Stream = function () {}
