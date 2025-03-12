832cf9c5084fef139bcc616700005cb775c03607
mbret <bret.maxime@gmail.com>
2025-03-13 00:15:20 +0100

    feat: added more docker

--- apps/api/src/covers.controller.ts
+++ apps/api/src/covers.controller.ts
@@ -14,6 +14,10 @@ import * as sharp from "sharp"
 
 const s3 = new S3Client({
   region: `us-east-1`,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+  },
 })
 
 @Controller("covers")
