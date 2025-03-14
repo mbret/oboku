b5febb7243a112124c4cf3059c93c6ce1f3c5fb5
mbret <bret.maxime@gmail.com>
2025-03-14 20:41:12 +0100

    fix: fixed cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,8 +4,11 @@ import { AppModule } from "./app.module"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors()
+  app.enableCors({
+    origin: "*",
+  })
 
   await app.listen(process.env.PORT ?? 3000)
 }
+
 bootstrap()
