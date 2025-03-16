b4aaa4cc157692dba6c802b7711892bc93f4eeb2
mbret <bret.maxime@gmail.com>
2025-03-16 23:34:00 +0100

    fix: throttle book metadata refetch

--- apps/api/src/features/books/BooksMedataService.ts
+++ apps/api/src/features/books/BooksMedataService.ts
@@ -1,16 +1,21 @@
 import { Injectable, Logger } from "@nestjs/common"
 import { ConfigService } from "@nestjs/config"
 import { EnvironmentVariables } from "src/types"
-import { refreshMetadataLongProcess } from "./metadata/refreshMetadataLongProcess"
-import { lock } from "src/lib/supabase/lock"
-import { createSupabaseClient } from "src/lib/supabase/client"
-
-const LOCK_MAX_DURATION_MN = 5
+import { getParametersValue } from "src/lib/ssm"
+import { configure } from "src/lib/plugins/google"
+import * as fs from "node:fs"
+import * as path from "node:path"
+import {
+  atomicUpdate,
+  findOne,
+  getNanoDbForUser,
+} from "src/lib/couch/dbHelpers"
+import { getAuthTokenAsync } from "src/lib/auth"
+import { PromiseReturnType } from "src/lib/types"
+import { retrieveMetadataAndSaveCover } from "../metadata/retrieveMetadataAndSaveCover"
 
 @Injectable()
 export class BooksMedataService {
-  private logger = new Logger(BooksMedataService.name)
-
   constructor(
     private readonly configService: ConfigService<EnvironmentVariables>,
   ) {}
@@ -22,40 +27,113 @@ export class BooksMedataService {
       authorization?: string
     },
   ) => {
-    const supabase = createSupabaseClient(this.configService)
+    const { bookId } = body
+    const { authorization } = headers
+    const credentials = JSON.parse(headers["oboku-credentials"] ?? "{}")
 
-    Logger.log(`invoke for ${body.bookId}`)
+    const [
+      client_id = ``,
+      client_secret = ``,
+      googleApiKey = ``,
+      jwtPrivateKey = ``,
+    ] = await getParametersValue({
+      Names: [
+        "GOOGLE_CLIENT_ID",
+        "GOOGLE_CLIENT_SECRET",
+        "GOOGLE_API_KEY",
+        "jwt-private-key",
+      ],
+      WithDecryption: true,
+    })
 
-    try {
-      const lockId = `metadata_${body.bookId}`
+    configure({
+      client_id,
+      client_secret,
+    })
 
-      const { alreadyLocked } = await lock(
-        lockId,
-        LOCK_MAX_DURATION_MN,
-        supabase,
+    const TMP_DIR_BOOKS = this.configService.getOrThrow("TMP_DIR_BOOKS", {
+      infer: true,
+    })
+
+    if (!this.configService.getOrThrow("OFFLINE", { infer: true })) {
+      const files = await fs.promises.readdir(TMP_DIR_BOOKS)
+
+      await Promise.all(
+        files.map((file) => {
+          return fs.promises.unlink(path.join(TMP_DIR_BOOKS, file))
+        }),
       )
-
-      if (!alreadyLocked) {
-        refreshMetadataLongProcess(
-          {
-            authorization: headers.authorization ?? "",
-            bookId: body.bookId,
-            rawCredentials: headers["oboku-credentials"] ?? "{}",
-          },
-          this.configService,
-          supabase,
-        ).catch(this.logger.error.bind(this.logger))
-
-        this.logger.log(`${body.bookId}: command sent with success`)
-      } else {
-        this.logger.log(`${body.bookId} is already locked, ignoring!`)
-      }
-    } catch (error) {
-      this.logger.error(error)
-
-      throw error
     }
 
-    return {}
+    const { name: userName } = await getAuthTokenAsync(
+      {
+        headers: {
+          authorization,
+        },
+      },
+      jwtPrivateKey,
+    )
+    const userNameHex = Buffer.from(userName).toString("hex")
+
+    const db = await getNanoDbForUser(
+      userName,
+      jwtPrivateKey,
+      this.configService.getOrThrow("COUCH_DB_URL", { infer: true }),
+    )
+
+    const book = await findOne("book", { selector: { _id: bookId } }, { db })
+
+    if (!book) throw new Error(`Unable to find book ${bookId}`)
+
+    if (book.metadataUpdateStatus !== "fetching") {
+      await atomicUpdate(db, "book", book._id, (old) => ({
+        ...old,
+        metadataUpdateStatus: "fetching" as const,
+      }))
+    }
+
+    const firstLinkId = (book.links || [])[0] || "-1"
+
+    const link = await findOne(
+      "link",
+      { selector: { _id: firstLinkId } },
+      { db },
+    )
+
+    if (!link) throw new Error(`Unable to find link ${firstLinkId}`)
+
+    let data: PromiseReturnType<typeof retrieveMetadataAndSaveCover>
+
+    try {
+      data = await retrieveMetadataAndSaveCover(
+        {
+          userName,
+          userNameHex,
+          credentials,
+          book,
+          link,
+          googleApiKey,
+          db,
+        },
+        this.configService,
+      )
+    } catch (e) {
+      await atomicUpdate(db, "book", book._id, (old) => ({
+        ...old,
+        metadataUpdateStatus: null,
+        lastMetadataUpdateError: "unknown",
+      }))
+
+      throw e
+    }
+
+    await Promise.all([
+      atomicUpdate(db, "link", link._id, (old) => ({
+        ...old,
+        contentLength: data.link.contentLength,
+      })),
+    ])
+
+    Logger.log(`lambda executed with success for ${book._id}`)
   }
 }
--- apps/api/src/features/books/books.controller.ts
+++ apps/api/src/features/books/books.controller.ts
@@ -1,11 +1,28 @@
-import { Body, Controller, Headers, Post } from "@nestjs/common"
+import { Body, Controller, Headers, Logger, Post } from "@nestjs/common"
 import { OnEvent } from "@nestjs/event-emitter"
 import { BooksMetadataRefreshEvent, Events } from "../../events"
 import { BooksMedataService } from "./BooksMedataService"
+import { InMemoryTaskQueueService } from "../queue/InMemoryTaskQueueService"
+import { from } from "rxjs"
 
 @Controller("books")
 export class BooksController {
-  constructor(private booksMedataService: BooksMedataService) {}
+  private logger = new Logger(BooksController.name)
+  private QUEUE_NAME = "books.metadata.refresh"
+
+  constructor(
+    private booksMedataService: BooksMedataService,
+    private readonly taskQueueService: InMemoryTaskQueueService,
+  ) {}
+
+  onModuleInit() {
+    this.taskQueueService.createQueue({
+      name: this.QUEUE_NAME,
+      maxConcurrent: 3,
+      deduplicate: true,
+      sequentialTasksWithSameId: true,
+    })
+  }
 
   @Post("metadata/refresh")
   async metadataRefresh(
@@ -15,18 +32,39 @@ export class BooksController {
       authorization?: string
     },
   ) {
-    return this.booksMedataService.refreshMetadata(body, headers)
+    this.logger.log("metadataRefresh", body.bookId)
+
+    this.taskQueueService.enqueue(
+      this.QUEUE_NAME,
+      () => from(this.booksMedataService.refreshMetadata(body, headers)),
+      {
+        id: body.bookId,
+      },
+    )
+
+    return {}
   }
 
   @OnEvent(Events.BOOKS_METADATA_REFRESH)
   async handleBooksMetadataRefresh(event: BooksMetadataRefreshEvent) {
-    this.booksMedataService.refreshMetadata(
+    this.logger.log("handleBooksMetadataRefresh", event.data.bookId)
+
+    this.taskQueueService.enqueue(
+      this.QUEUE_NAME,
+      () =>
+        from(
+          this.booksMedataService.refreshMetadata(
+            {
+              bookId: event.data.bookId,
+            },
+            {
+              authorization: event.data.authorization,
+              "oboku-credentials": JSON.stringify(event.data.obokuCredentials),
+            },
+          ),
+        ),
       {
-        bookId: event.data.bookId,
-      },
-      {
-        authorization: event.data.authorization,
-        "oboku-credentials": JSON.stringify(event.data.obokuCredentials),
+        id: event.data.bookId,
       },
     )
   }
