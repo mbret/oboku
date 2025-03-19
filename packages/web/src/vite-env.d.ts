61255c90494ba964d2275bece150445c8ea0e41f
mbret <bret.maxime@gmail.com>
2025-03-19 22:06:16 +0100

    fix: fixed env

--- packages/web/src/vite-env.d.ts
+++ packages/web/src/vite-env.d.ts
@@ -1,2 +1,18 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL?: string
+  /**
+   * couchdb is used for users database
+   */
+  readonly VITE_API_COUCH_URI?: string
+  /**
+   * firebase is used for tracking and analysis
+   */
+  readonly VITE_FIREBASE_CONFIG?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
