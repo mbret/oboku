a4260c8a594fb603a0e710554ee10906b25102c0
mbret <bret.maxime@gmail.com>
2025-03-29 23:18:48 +0100

    fix: fixed lint

--- apps/api/src/features/web/web.controller.ts
+++ apps/api/src/features/web/web.controller.ts
@@ -1,18 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
-import type { AppConfigService } from "../../config/AppConfigService";
-import { Public } from "src/auth/auth.guard";
+import { Controller, Get } from "@nestjs/common"
+import { AppConfigService } from "../../config/AppConfigService"
+import { Public } from "src/auth/auth.guard"
 
 @Controller("web")
 export class WebController {
-	constructor(private readonly appConfigService: AppConfigService) {}
+  constructor(private readonly appConfigService: AppConfigService) {}
 
-	@Public()
-	@Get("config")
-	getConfig() {
-		return {
-			GOOGLE_CLIENT_ID: this.appConfigService.GOOGLE_CLIENT_ID,
-			GOOGLE_API_KEY: this.appConfigService.GOOGLE_API_KEY,
-			DROPBOX_CLIENT_ID: this.appConfigService.DROPBOX_CLIENT_ID,
-		};
-	}
+  @Public()
+  @Get("config")
+  getConfig() {
+    return {
+      GOOGLE_CLIENT_ID: this.appConfigService.GOOGLE_CLIENT_ID,
+      GOOGLE_API_KEY: this.appConfigService.GOOGLE_API_KEY,
+      DROPBOX_CLIENT_ID: this.appConfigService.DROPBOX_CLIENT_ID,
+    }
+  }
 }
