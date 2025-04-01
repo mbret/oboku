a3ad47530f5a871f0ac8ff3d4fefe3e380db6f2f
mbret <bret.maxime@gmail.com>
2025-04-01 23:26:36 +0200

    fix: lint

--- apps/web/src/pages/collections/CollectionDetailsScreen/EmptyList.tsx
+++ apps/web/src/pages/collections/CollectionDetailsScreen/EmptyList.tsx
@@ -3,41 +3,41 @@ import EmptyLibraryAsset from "../../../assets/empty-library.svg"
 import { memo } from "react"
 
 export const EmptyList = memo(() => {
-    const theme = useTheme()
-  
-    return (
+  const theme = useTheme()
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        flex: 1,
+      }}
+    >
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
           flex: 1,
+          justifyContent: "center",
+          flexFlow: "column",
+          alignItems: "center",
+          textAlign: "center",
+          width: "80%",
+          maxWidth: theme.custom.maxWidthCenteredContent,
         }}
       >
-        <div
+        <img
           style={{
-            display: "flex",
-            flex: 1,
-            justifyContent: "center",
-            flexFlow: "column",
-            alignItems: "center",
-            textAlign: "center",
-            width: "80%",
-            maxWidth: theme.custom.maxWidthCenteredContent,
+            width: "100%",
           }}
-        >
-          <img
-            style={{
-              width: "100%",
-            }}
-            src={EmptyLibraryAsset}
-            alt="libray"
-          />
-          <Typography style={{ maxWidth: 300, paddingTop: theme.spacing(1) }}>
-            It looks like your library is empty for the moment. Maybe it's time to
-            add a new book
-          </Typography>
-        </div>
+          src={EmptyLibraryAsset}
+          alt="libray"
+        />
+        <Typography style={{ maxWidth: 300, paddingTop: theme.spacing(1) }}>
+          It looks like your library is empty for the moment. Maybe it's time to
+          add a new book
+        </Typography>
       </div>
-    )
-  })
+    </div>
+  )
+})
