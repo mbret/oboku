3b94886eeaf42141ddbe44e7b7fda714830c3ddf
mbret <bret.maxime@gmail.com>
2025-06-17 22:04:50 +0200

    feat: upgrade admin

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -7,7 +7,6 @@ import { routeTree } from "./routeTree.gen"
 
 import "./styles.css"
 import "@mantine/core/styles.css"
-import reportWebVitals from "./reportWebVitals.ts"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { MantineProvider } from "@mantine/core"
 
@@ -44,8 +43,3 @@ if (rootElement && !rootElement.innerHTML) {
     </StrictMode>,
   )
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
