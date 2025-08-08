70e6bb0ee9f79c91aa2cd4412f6cd290a75ce4ee
mbret <bret.maxime@gmail.com>
2025-08-08 17:05:58 +0200

    fix: oupsie

--- apps/web/src/pages/LibraryCollectionScreen.tsx
+++ apps/web/src/pages/LibraryCollectionScreen.tsx
@@ -1,18 +1,13 @@
 import { useState, useMemo, useCallback, memo } from "react"
 import { Button, Stack, Toolbar as MuiToolbar } from "@mui/material"
-import { useNavigate } from "react-router"
 import { CollectionList } from "../collections/lists/CollectionList"
 import { useSignalValue } from "reactjrx"
 import { useLibraryShelves } from "../library/shelves/useLibraryShelves"
 import { Toolbar } from "../library/shelves/Toolbar"
 import { libraryShelvesFiltersSignal } from "../library/shelves/filters/states"
-import type { CollectionDocType } from "@oboku/shared"
-import type { DeepReadonlyObject } from "rxdb"
 import { AddCollectionDialog } from "../library/shelves/AddCollectionDialog"
-import { ROUTES } from "../navigation/routes"
 
 export const LibraryCollectionScreen = memo(() => {
-  const navigate = useNavigate()
   const [isAddCollectionDialogOpened, setIsAddCollectionDialogOpened] =
     useState(false)
   const { viewMode } = useSignalValue(
@@ -41,13 +36,6 @@ export const LibraryCollectionScreen = memo(() => {
 
   const listRenderHeader = useCallback(() => listHeader, [listHeader])
 
-  const onItemClick = useCallback(
-    (item: DeepReadonlyObject<CollectionDocType>) => {
-      navigate(ROUTES.COLLECTION_DETAILS.replace(":id", item._id))
-    },
-    [navigate],
-  )
-
   return (
     <Stack flex={1} overflow="hidden">
       <Toolbar
@@ -66,7 +54,6 @@ export const LibraryCollectionScreen = memo(() => {
           }}
           data={collections}
           renderHeader={listRenderHeader}
-          onItemClick={onItemClick}
           viewMode={viewMode}
           restoreScrollId="LibraryCollectionScreen"
         />
