55f3be44224daab62eaf231edb4839fb6b747f6e
mbret <bret.maxime@gmail.com>
2026-04-15 10:53:04 +0200

    feat: cleanup

--- apps/web/src/books/BookCard/BookCardHorizontal.tsx
+++ apps/web/src/books/BookCard/BookCardHorizontal.tsx
@@ -1,5 +1,5 @@
 import { Box, Chip, Stack, Typography, useTheme } from "@mui/material"
-import { type FC, memo } from "react"
+import { memo } from "react"
 import { useBook, useIsBookProtected } from "../states"
 import { ReadingStateState } from "@oboku/shared"
 import {
@@ -21,7 +21,14 @@ import { useBookDownloadState } from "../../download/states"
 import { useLink } from "../../links/states"
 import { pluginsByType } from "../../plugins/configure"
 
-export const BookCardHorizontal: FC<{
+export const BookCardHorizontal = memo(function BookCardHorizontal({
+  bookId,
+  size = "large",
+  withDrawerActions = true,
+  withCover = true,
+  withAuthors = true,
+  withDownloadIcons = false,
+}: {
   bookId: string
   isSelected?: (id: string) => boolean
   size?: "small" | "large"
@@ -29,103 +36,112 @@ export const BookCardHorizontal: FC<{
   withCover?: boolean
   withAuthors?: boolean
   withDownloadIcons?: boolean
-}> = memo(
-  ({
-    bookId,
-    size = "large",
-    withDrawerActions = true,
-    withCover = true,
-    withAuthors = true,
-    withDownloadIcons = false,
-  }) => {
-    const { data: book } = useBook({
-      id: bookId,
-    })
-    const { data: link } = useLink({ id: book?.links?.[0] })
-    const linkPlugin = link?.type ? pluginsByType[link?.type] : undefined
-    const theme = useTheme()
-    const bookDownloadState = useBookDownloadState(bookId)
-    const { data: isBookProtected } = useIsBookProtected(book)
-    const metadata = getMetadataFromBook(book)
+}) {
+  const { data: book } = useBook({
+    id: bookId,
+  })
+  const { data: link } = useLink({ id: book?.links?.[0] })
+  const linkPlugin = link?.type ? pluginsByType[link?.type] : undefined
+  const theme = useTheme()
+  const bookDownloadState = useBookDownloadState(bookId)
+  const { data: isBookProtected } = useIsBookProtected(book)
+  const metadata = getMetadataFromBook(book)
 
-    return (
-      <>
-        {withCover && (
-          <BookCoverCard
-            bookId={bookId}
-            style={{
-              marginRight: theme.spacing(1),
-            }}
-            withBadges={false}
-          />
-        )}
-        <Stack
+  return (
+    <>
+      {withCover && (
+        <BookCoverCard
+          bookId={bookId}
           style={{
+            marginRight: theme.spacing(1),
+          }}
+          withBadges={false}
+        />
+      )}
+      <Stack
+        style={{
+          flex: 1,
+          minHeight: 0,
+          flexDirection: "column",
+          overflow: "hidden",
+        }}
+      >
+        <Typography
+          noWrap
+          variant="body1"
+          display="block"
+          {...(size === "small" && {
+            variant: "body2",
+          })}
+        >
+          {metadata?.title || "Unknown"}
+        </Typography>
+        {withAuthors && (
+          <Typography noWrap color="textSecondary" variant="body2">
+            {(metadata?.authors ?? [])[0] || "Unknown"}
+          </Typography>
+        )}
+        <Box
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
             flex: 1,
-            minHeight: 0,
-            flexDirection: "column",
-            overflow: "hidden",
+            alignItems: "flex-end",
           }}
         >
-          <Typography
-            noWrap
-            variant="body1"
-            display="block"
-            {...(size === "small" && {
-              variant: "body2",
-            })}
-          >
-            {metadata?.title || "Unknown"}
-          </Typography>
-          {withAuthors && (
-            <Typography noWrap color="textSecondary" variant="body2">
-              {(metadata?.authors ?? [])[0] || "Unknown"}
-            </Typography>
-          )}
-          <Box
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              flex: 1,
-              alignItems: "flex-end",
-            }}
-          >
-            <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
-              {linkPlugin?.Icon && (
-                <linkPlugin.Icon fontSize="small" sx={{ display: "block" }} />
-              )}
-              {withDownloadIcons &&
-                (bookDownloadState?.isDownloading ? (
-                  <DownloadingRounded color="action" />
-                ) : bookDownloadState?.isDownloaded ? (
-                  <CloudDoneRounded color="action" />
-                ) : (
-                  <CloudDownloadRounded color="action" />
-                ))}
-              {isBookProtected && <NoEncryptionRounded color="action" />}
-              {book?.isNotInterested && <ThumbDownOutlined color="action" />}
-              {book?.readingStateCurrentState ===
-                ReadingStateState.Finished && <DoneRounded color="action" />}
-              {book?.readingStateCurrentState === ReadingStateState.Reading && (
-                <Box
-                  display="flex"
-                  flexDirection="row"
-                  alignItems="center"
-                  gap={0.5}
-                >
-                  <MenuBookRounded color="action" />
-                  <Typography variant="body2">
-                    {Math.floor(
-                      (book?.readingStateCurrentBookmarkProgressPercent || 0) *
-                        100,
-                    ) || 1}
-                    %
-                  </Typography>
-                </Box>
-              )}
-            </Box>
-            <div style={{ display: "flex", flexDirection: "row" }}>
-              {book?.metadataUpdateStatus === "fetching" && (
+          <Box display="flex" flexDirection="row" alignItems="center" gap={1}>
+            {linkPlugin?.Icon && (
+              <linkPlugin.Icon fontSize="small" sx={{ display: "block" }} />
+            )}
+            {withDownloadIcons &&
+              (bookDownloadState?.isDownloading ? (
+                <DownloadingRounded color="action" />
+              ) : bookDownloadState?.isDownloaded ? (
+                <CloudDoneRounded color="action" />
+              ) : (
+                <CloudDownloadRounded color="action" />
+              ))}
+            {isBookProtected && <NoEncryptionRounded color="action" />}
+            {book?.isNotInterested && <ThumbDownOutlined color="action" />}
+            {book?.readingStateCurrentState === ReadingStateState.Finished && (
+              <DoneRounded color="action" />
+            )}
+            {book?.readingStateCurrentState === ReadingStateState.Reading && (
+              <Box
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+                gap={0.5}
+              >
+                <MenuBookRounded color="action" />
+                <Typography variant="body2">
+                  {Math.floor(
+                    (book?.readingStateCurrentBookmarkProgressPercent || 0) *
+                      100,
+                  ) || 1}
+                  %
+                </Typography>
+              </Box>
+            )}
+          </Box>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            {book?.metadataUpdateStatus === "fetching" && (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  marginLeft: theme.spacing(1),
+                }}
+              >
+                <Chip
+                  size="small"
+                  avatar={<LoopRounded className="oboku-infinite-spin" />}
+                  label="metadata..."
+                />
+              </div>
+            )}
+            {book?.metadataUpdateStatus !== "fetching" &&
+              !!book?.lastMetadataUpdateError && (
                 <div
                   style={{
                     display: "flex",
@@ -135,50 +151,33 @@ export const BookCardHorizontal: FC<{
                 >
                   <Chip
                     size="small"
-                    avatar={<LoopRounded className="oboku-infinite-spin" />}
-                    label="metadata..."
+                    icon={<ErrorRounded color="primary" />}
+                    color="primary"
+                    label="metadata"
                   />
                 </div>
               )}
-              {book?.metadataUpdateStatus !== "fetching" &&
-                !!book?.lastMetadataUpdateError && (
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      marginLeft: theme.spacing(1),
-                    }}
-                  >
-                    <Chip
-                      size="small"
-                      icon={<ErrorRounded color="primary" />}
-                      color="primary"
-                      label="metadata"
-                    />
-                  </div>
-                )}
-            </div>
-          </Box>
+          </div>
+        </Box>
+      </Stack>
+      {withDrawerActions && (
+        <Stack
+          justifyContent="center"
+          width={[40, 50]}
+          flexDirection="row"
+          style={{
+            alignItems: "center",
+            marginLeft: theme.spacing(1),
+          }}
+          onClick={(e) => {
+            e.stopPropagation()
+            book?._id &&
+              bookActionDrawerSignal.setValue({ openedWith: book._id })
+          }}
+        >
+          <MoreVert />
         </Stack>
-        {withDrawerActions && (
-          <Stack
-            justifyContent="center"
-            width={[40, 50]}
-            flexDirection="row"
-            style={{
-              alignItems: "center",
-              marginLeft: theme.spacing(1),
-            }}
-            onClick={(e) => {
-              e.stopPropagation()
-              book?._id &&
-                bookActionDrawerSignal.setValue({ openedWith: book._id })
-            }}
-          >
-            <MoreVert />
-          </Stack>
-        )}
-      </>
-    )
-  },
-)
+      )}
+    </>
+  )
+})
