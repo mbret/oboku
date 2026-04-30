c01e1d0886d13d87a0c169efa1d8d6c137d2f148
mbret <bret.maxime@gmail.com>
2026-04-30 17:04:58 +0200

    feat: cleanup

--- apps/web/src/common/forms/ControlledSecretSelect.tsx
+++ apps/web/src/common/forms/ControlledSecretSelect.tsx
@@ -1,42 +1,75 @@
-import type { ComponentProps } from "react"
-import type { FieldPath, FieldValues } from "react-hook-form"
-import { ControlledTextFieldSelect } from "./ControlledTextFieldSelect"
-import { useSecrets } from "../../secrets/useSecrets"
 import { KeyRounded } from "@mui/icons-material"
-import { InputAdornment } from "@mui/material"
+import {
+  InputAdornment,
+  MenuItem,
+  TextField,
+  type TextFieldProps,
+} from "@mui/material"
+import type { ComponentProps } from "react"
+import { Controller, type FieldPath, type FieldValues } from "react-hook-form"
+import { useSecrets } from "../../secrets/useSecrets"
+import { errorToHelperText } from "./errorToHelperText"
 
 export const ControlledSecretSelect = <
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
   TTransformedValues = TFieldValues,
 >({
-  ...rest
+  control,
+  name,
+  rules,
+  helperText = "Select a secret to use",
+  slotProps = {
+    input: {
+      startAdornment: (
+        <InputAdornment position="start">
+          <KeyRounded />
+        </InputAdornment>
+      ),
+    },
+  },
+  ...selectProps
 }: Omit<
-  ComponentProps<
-    typeof ControlledTextFieldSelect<TFieldValues, TName, TTransformedValues>
-  >,
-  "options"
->) => {
+  ComponentProps<typeof Controller<TFieldValues, TName, TTransformedValues>>,
+  "render"
+> &
+  TextFieldProps) => {
   const { data: secrets = [] } = useSecrets()
 
+  const options = secrets.map((secret) => ({
+    label: secret.name,
+    value: secret._id,
+    id: secret._id,
+  }))
+
   return (
-    <ControlledTextFieldSelect
-      options={secrets?.map((secret) => ({
-        label: secret.name,
-        value: secret._id,
-        id: secret._id,
-      }))}
-      slotProps={{
-        input: {
-          startAdornment: (
-            <InputAdornment position="start">
-              <KeyRounded />
-            </InputAdornment>
-          ),
-        },
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({ field: { ref, ...rest }, fieldState }) => {
+        return (
+          <TextField
+            {...rest}
+            {...selectProps}
+            select
+            inputRef={ref}
+            slotProps={slotProps}
+            error={fieldState.invalid}
+            helperText={
+              fieldState.invalid
+                ? errorToHelperText(fieldState.error)
+                : helperText
+            }
+          >
+            {options.map((option) => (
+              <MenuItem key={option.id} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        )
       }}
-      helperText="Select a secret to use"
-      {...rest}
     />
   )
 }
